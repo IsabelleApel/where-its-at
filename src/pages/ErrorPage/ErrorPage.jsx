0d6d6ce4af68fea6fb89562506da4c54ad8efd79
Isabelle Apelgren <isabelleapelgren@gmail.com>
2025-05-09 23:14:37 +0200

    error-page css

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,11 @@
+import './errorPage.css';
 
 function ErrorPage() {
   return (
-    <p>The page you're trying to find does not exist</p>
+    <section className="page error-page">
+      <p className="info-text">The page you're trying to find does not exist</p>
+    </section>
+    
   )
 }
 
